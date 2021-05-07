d8835d27dbaf31880596162e068a82aaed8e0688
yangwen <whutyw@126.com>
2021-05-07 22:28:51 +0800

    Q1

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package org.javaschool;
+
+public class HelloWorld{
+	
+	private int m;
+	
+	public int inc() {
+		return m + 1;
+	}
+	
+	public static void main(String[] args) {
+		HelloWorld obj = new HelloWorld();
+	}
+}
